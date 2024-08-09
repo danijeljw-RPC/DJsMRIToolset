b1fa078f93f710269893504151892775e8bb6dc9
Danijel-James Wynyard <dwynyard@judgejudy.local>
2024-08-09 23:08:27 +1000

    init commit

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace DJsMRIToolset
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
